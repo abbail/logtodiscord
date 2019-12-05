c0679871c224a6e76d2bf3edbf58dc2236bba9a0
Jeffery Nelson <jeffery.nelson@csi.corp>
2019-12-05 08:26:24 -0600

    added line to app.ts for testing

--- app/app.ts
+++ app/app.ts
@@ -5,6 +5,12 @@ import { SQLManager } from './sql-manager';
 import config from '../config.json';
 
 const sqlManager = new SQLManager(config.sqlite3DBPath);
-const logManager = new LogManager(config.logFilePath).logStream;
+const logManager = new LogManager(config.logFilePath);
 const chatManager: ChatManager = new ChatManager(config.token, sqlManager);
-const auctionWatcher: AuctionWatcher = new AuctionWatcher(sqlManager, logManager, chatManager);
+const auctionWatcher: AuctionWatcher = new AuctionWatcher(sqlManager, logManager.logStream, chatManager);
+
+/*
+setTimeout(() => {
+  logManager.handleNewLogLine('[Wed Nov 27 10:53:58 2019] Tester auctions, \'WTB Brazen Brass Kilij or some junk.  Staff of Writhing, Cloak of Flames, Bronze Dagger\'');
+}, 1500);
+*/
